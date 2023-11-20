8f27634e7568247b8fa1b961b3e23ad9f5343e46
Jitesh4565 <73875204+Jitesh4565@users.noreply.github.com>
2023-11-20 18:15:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function enter()
+	{
+		let para=document.getElementById('status');
+		let h1=document.createElement('h1');
+		let btn=document.getElementById('enterBtn');
+		h1.innerText='Entered Metaverse';
+		para.remove();
+	    btn.parentNode.insertBefore(h1, btn);
+     
+	   
+	}
